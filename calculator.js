ea99b51414fdb9be62e9384aa82ec9e206b2fe20
Marcelo Llanos <mrcl562@gmail.com>
2022-11-17 00:55:05 -0400

    style changes now in green and JS conditional

--- calculator.js
+++ calculator.js
@@ -21,16 +21,15 @@ buttons.map(button => {
 })
 
 document.addEventListener('keyup', (event) => {
-    if(event.key == "Enter"){
-        equal();
-    }
-    if(event.key == "Delete"){
-        clear();
-    }
+    event.key == "Enter"
+        ?equal():event.key == "Delete"
+            ?clear(): false;
+    
     if(event.key === "1"|| event.key === "2"|| event.key === "3"|| event.key === "4"|| event.key === "5"|| event.key === "6"|| event.key === "7"|| event.key === "8"|| event.key === "9"|| event.key === "+"|| event.key === "-"|| event.key === "/"|| event.key === "*"|| event.key === "("|| event.key === ")"){
         display.innerText += event.key;
     }
   }, false);
+
   document.addEventListener("keydown", (event) => {
     if(event.key == "Backspace"){
         backSpace();
