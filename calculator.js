4352f9266962b3198a83abeeabfc0fa6417291d3
Marcelo Llanos <mrcl562@gmail.com>
2022-11-23 10:03:24 -0400

    JS fixed functions and cleaner code

--- calculator.js
+++ calculator.js
@@ -1,51 +1,50 @@
-let display = document.querySelector("#display");
-let buttons = Array.from(document.getElementsByClassName("button"));
+const display = document.querySelector("#display");
+const buttons = Array.from(document.querySelectorAll(".button"));
+const error_msg = "ERROR"
+const characters = {
+    "1": "1", "2": "2", "3": "3", "4": "4",
+    "5": "5", "6": "6", "7": "7", "8": "8",
+    "9": "9", "0": "0", "(": "(", ")": ")",
+    "/": "/", "*": "*", "-": "-", "+": "+"
+};
 
-
+//button displaying
 buttons.map(button => {
     button.addEventListener("click" , (ev) =>{
-        switch(ev.target.innerText){
-            case "C":
-                clear();
-                break;
-            case "del":
-                backSpace();
-                break;
-            case "=":
-                equal();
-                break;
-            default:
-                display.innerText += ev.target.innerText;
-        }    
+        display.innerText == error_msg || ev.target.innerText == "C"
+            ? clear() : ev.target.innerText == "del"
+                ? backSpace() : ev.target.innerText == "="
+                    ? equal() : display.innerText += ev.target.innerText;        
     })
 })
 
+//keyboard displaying
 document.addEventListener('keyup', (event) => {
-    event.key == "Enter"
-        ?equal():event.key == "Delete"
-            ?clear(): false;
-    
-    if(event.key === "1"|| event.key === "2"|| event.key === "3"|| event.key === "4"|| event.key === "5"|| event.key === "6"|| event.key === "7"|| event.key === "8"|| event.key === "9"|| event.key === "+"|| event.key === "-"|| event.key === "/"|| event.key === "*"|| event.key === "("|| event.key === ")"){
-        display.innerText += event.key;
-    }
-  }, false);
-
-  document.addEventListener("keydown", (event) => {
-    if(event.key == "Backspace"){
-        backSpace();
-    }
-  })
+    display.innerText == error_msg || event.key == "Delete"
+        ? clear() : event.key == "Enter"
+            ? equal() : event.key == characters[event.key]
+                ? display.innerText += event.key : false;
+})
+//keyboard backspace call
+document.addEventListener("keydown", (event) => {
+    display.innerText == error_msg
+        ? clear() : event.key == "Backspace" 
+            ? backSpace() : false;
+})
 
 function equal(){
     try{
         display.innerText = eval(display.innerText);
     }
     catch{
-        display.innerText = "WTF!!"}
+        display.innerText = error_msg;
+    }
 }
+
 function clear(){
     display.innerText = "";
 }
+
 function backSpace(){
     display.innerText = display.innerText.slice(0,-1);
 }
